01b8175e81524e6966733329346dba23142f9ced
Robbie Bendick <robbiebendick@gmail.com>
2021-07-01 18:08:26 -0700

    fix same-class markers in arena

--- core.lua
+++ core.lua
@@ -16,218 +16,62 @@ frame:RegisterEvent("CHAT_MSG_BG_SYSTEM_NEUTRAL")
 --]]
 
 --[[
-    TODO: Recursive function &/or duplicates not working
-          Breaks when setting assist
+    TODO: Breaks when setting assist
+          Marks self at 15 seconds instead of 30
 --]]
 
-classes = {
-    ["Rogue"] = 1,
-    ["Druid"] = 2,
-    ["Warlock"] = 3,
-    ["Paladin"] = 3,
-    ["Hunter"] = 4,
-    ["Mage"] = 5,
-    ["Shaman"] = 6,
-    ["Warrior"] = 7,
-    ["Priest"] = 8,
-}
-
-
-usedMarkers = {
-    star = false,
-    circle = false,
-    diamond = false,
-    triangle = false,
-    moon = false,
-    square = false,
-    cross = false,
-    skull = false
-}
-priority = {}
-duplicates = {}
-MissingTable = {}
+unused_markers = {1,2,3,4,5,6,7,8}  
 
 function tablelength(T)
     local count = 0
     for _ in pairs(T) do
         count = count + 1
     end
-    print(count)
     return count
 end
 
-local function chooseMarker(target, duplicates, table, i, ...)
-    if i == 1 then
-        if not table.star then
-            SetRaidTarget(target, i)
-            table.star = true
-        else
-            table.insert(duplicates, i)
+local function chooseMarker(target, i, ...)
+    -- check if mark is in unused_markers
+    local item;
+    for v in pairs(unused_markers) do
+        if v == i then
+            item = v
         end
     end
-    if i == 2 then
-        if not table.circle then
-            SetRaidTarget(target, i)
-            table.circle = true
-        else          
-            table.insert(duplicates, i)
-        end
-    end
-    if i == 3 then
-        if not table.diamond then
-            SetRaidTarget(target, i)
-            table.diamond = true
-        else
-            table.insert(duplicates, i)
-        end
-    end
-    if i == 4 then
-        if not table.triangle then
-            SetRaidTarget(target, i)
-            table.triangle = true
-        else
-            table.insert(duplicates, i)
-        end
-    end
-    if i == 5 then
-        if not table.moon then
-            SetRaidTarget(target, i)
-            table.moon = true
-        else
-            table.insert(duplicates, i)
-        end
-    end
-    if i == 6 then
-        if not table.square then
-            SetRaidTarget(target, i)
-            table.square = true
-        else
-            table.insert(duplicates, i)
-        end
-    end
-    if i == 7 then
-        if not table.cross then
-            SetRaidTarget(target, i)
-            table.cross = true
-        else
-            table.insert(duplicates, i)
-        end
-    end
-    if i == 8 then
-        if not table.skull then
-            SetRaidTarget(target, 8)
-            table.skull = true
-        else
-            table.insert(duplicates, i)
-        end
-    end
+    SetRaidTarget(target, item)
+    table.remove(unused_markers, item)
 end
 
-local function setRaidTargetByClass(target, duplicates, table, i, ...)
+local function setRaidTargetByClass(target, i, ...)
     _, englishClass, _ = UnitClass(target);
     print("target: " .. target .. "\nclass: " .. englishClass)
-
+    
     if englishClass == "ROGUE" then
-        chooseMarker(target, duplicates, table, 1)
+        chooseMarker(target, 1)
     end
     if englishClass == "DRUID" then
-        chooseMarker(target, duplicates, table, 2)
+        chooseMarker(target, 2)
     end
     if englishClass == "WARLOCK" then
-        chooseMarker(target, duplicates, table, 3)
+        chooseMarker(target, 3)
     end
     if englishClass == "PALADIN" then
-        chooseMarker(target, duplicates, table, 3)
+        chooseMarker(target, 3)
     end
     if englishClass == "HUNTER" then
-        chooseMarker(target, duplicates, table, 4)
+        chooseMarker(target, 4)
     end
     if englishClass == "MAGE" then
-        chooseMarker(target, duplicates, table, 5)
+        chooseMarker(target, 5)
     end
     if englishclass == "SHAMAN" then
-        chooseMarker(target, duplicates, table, 6)
+        chooseMarker(target, 6)
     end
     if englishClass == "WARRIOR" then
-        chooseMarker(target, duplicates, table, 7)
+        chooseMarker(target, 7)
     end
     if englishClass == "PRIEST" then
-        chooseMarker(target, duplicates, table, 8)
-    end
-end
-
-local function recursiveChooseMarker(target, duplicates, table, i, ...)
-    if i == 1 then
-        if not table.star then
-            SetRaidTarget(target, i)
-            table.star = true
-        else
-            recursiveChooseMarker(target, duplicates, table, i + 1)
-        end
-    end
-    if i == 2 then
-        if not table.circle then
-            SetRaidTarget(target, i)
-            table.circle = true
-        else
-            recursiveChooseMarker(target, duplicates, table, i + 1)
-        end
-    end
-    if i == 3 then
-        if not table.diamond then
-            SetRaidTarget(target, i)
-            table.diamond = true
-        else
-            recursiveChooseMarker(target, duplicates, table, i + 1)
-        end
-    end
-    if i == 4 then
-        if not table.triangle then
-            SetRaidTarget(target, i)
-            table.triangle = true
-        else
-            recursiveChooseMarker(target, duplicates, table, i + 1)
-        end
-    end
-    if i == 5 then
-        if not table.moon then
-            SetRaidTarget(target, i)
-            table.moon = true
-        else
-            recursiveChooseMarker(target, duplicates, table, i + 1)
-        end
-    end
-    if i == 6 then
-        if not table.square then
-            SetRaidTarget(target, i)
-            table.square = true
-        else
-            recursiveChooseMarker(target, duplicates, table, i + 1)
-        end
-    end
-    if i == 7 then
-        if not table.cross then
-            SetRaidTarget(target, i)
-            table.cross = true
-        else
-            recursiveChooseMarker(target, duplicates, table, i + 1)
-        end
-    end
-    if i == 8 then
-        if not table.skull then
-            SetRaidTarget(target, i)
-            table.skull = true
-        end
-    end
-end
-
-
-function getMissingMarkers(table, minNumber, maxNumber)
-    for i = minNumber, maxNumber do
-        -- check which marks are set to false
-        if not table.star then
-        table.insert(MissingTable, i)
-        end
+        chooseMarker(target, 8)
     end
 end
 
@@ -243,22 +87,23 @@ local function markAndRaidAssistTeammates(self, event, ...)
                 if UnitIsGroupLeader("player") then
                     ConvertToRaid()
                     -- mark player
-                    setRaidTargetByClass("player", duplicates, usedMarkers, i)
-                    for i = 1, members do
-                        -- give assist
+                    if GetRaidTargetIndex("player") == nil then
+                        setRaidTargetByClass("player")
+                    end
+                    for i = 1, members-1 do
                         -- mark party members
-                        print("No marker on party" .. i)
-                        setRaidTargetByClass("party" .. i, duplicates, usedMarkers, i)
+                        if GetRaidTargetIndex("party"..i) == nil then
+                            setRaidTargetByClass("party" .. i)
+                        end
+                        -- give assist
                         -- SendChatMessage("/assist " .. UnitName(target))
                     end
-                    numDuplicates = tablelength(duplicates)
-                    print(numDuplicates)
-                    if numDuplicates > 0 then
-                        getMissingMarkers(usedMarkers, 1, numDuplicates);
-                        for i=1, members do
-                            if GetRaidTargetIndex("party"..i) == nil then
-                                SetRaidTarget("party"..i, MissingTable[i])
-                            end
+                    for i=1, members-1 do
+                        local randomIndexOfTable = math.random(tablelength(unused_markers))
+                        if GetRaidTargetIndex("party"..i) == nil then
+                            print("RandomIndexOfTable: " .. randomIndexOfTable)
+                            SetRaidTarget("party"..i, unused_markers[randomIndexOfTable])
+                            table.remove(randomIndexOfTable)
                         end
                     end
                 end
