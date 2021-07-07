5ad917a06e22f737b75d502346d2b19c7915c4f2
Robbie Bendick <robbiebendick@gmail.com>
2021-07-07 03:36:08 -0700

    clean code up

--- core.lua
+++ core.lua
@@ -9,15 +9,10 @@ frame:RegisterEvent("CHAT_MSG_BG_SYSTEM_NEUTRAL")
         2 = Orange Circle; Druid
         3 = Purple Diamond; Lock, Paladin
         4 = Green Triangle; Hunter
-        5 = White Crescent Moon; Priest, Mage
-        6 = Blue Square; Mage, Shaman
-        7 = Red "X" Cross; Filler?
-        8 = White Skull; Warrior
---]]
-
---[[
-    TODO: When there's 2 of the same classes, and you happen to be one of those two players, places mark on self first, then instantly swaps to party1, then vice-versa at 15 seconds.
-          Still need to find a way to give raid-assistant to party members.
+        5 = White Crescent Moon; Mage
+        6 = Blue Square; Shaman
+        7 = Red "X" Cross; Warrior
+        8 = White Skull; Priest
 --]]
 
 unused_markers = {1,2,3,4,5,6,7,8}  
@@ -30,11 +25,17 @@ function tablelength(T)
     return count
 end
 
+function sleep(n)
+    local t = os.clock()
+    while os.clock() - t <= n do
+      -- nothing
+    end
+end
+
 local function chooseMarker(target, i, ...)
     -- check if the marker we are going to use is available
     for v in pairs(unused_markers) do
         if v == i then
-            print("[ChooseMarker] Value:"..v.."[ChooseMarker] Item:"..i)
             SetRaidTarget(target, v)
             table.remove(unused_markers, v)
         end
@@ -42,8 +43,7 @@ local function chooseMarker(target, i, ...)
 end
 
 local function setRaidTargetByClass(target, i, ...)
-    _, englishClass, _ = UnitClass(target);
-    print("target: " .. target .. "\nclass: " .. englishClass)
+    local _, englishClass, _ = UnitClass(target);
     
     if englishClass == "ROGUE" then
         chooseMarker(target, 1)
@@ -74,46 +74,30 @@ local function setRaidTargetByClass(target, i, ...)
     end
 end
 
-
-function sleep(n)
-    local t = os.clock()
-    while os.clock() - t <= n do
-      -- nothing
-    end
-end
-
-
-local function markAndRaidAssistTeammates(self, event, ...)
-    local members = GetNumGroupMembers()
-    
+local function markTeammatesAndSelf(self, event, ...)
     if event == "CHAT_MSG_BG_SYSTEM_NEUTRAL" then
         arg1 = ...
         if (string.find(arg1, "Thirty seconds until the Arena battle begins!") or
             string.find(arg1, "Fifteen seconds until the Arena battle begins!")) then
-            members = GetNumGroupMembers()
+            local members = GetNumGroupMembers()
             if members > 1 then
                 if UnitIsGroupLeader("player") then
                     ConvertToRaid()
                     -- mark self
                     if GetRaidTargetIndex("player") == nil then
-                        print("marking myself")
                         setRaidTargetByClass("player")
                     end
                     for i = 1, members-1 do
                         -- mark party members                        
                         if GetRaidTargetIndex("party"..i) == nil then
-                            print("marker on party" .. i .. " should be out")
-                            setRaidTargetByClass("party" .. i)
+                            setRaidTargetByClass("party"..i)
                         end
-                        -- SendChatMessage("/assist " .. UnitName(target))
                     end
-
-                    -- delay 2 seconds, or else GetRaidTargetIndex will return nil and it will instantly throw a random mark on party members.
-                    sleep(2)
+                    -- delay for a second, else GetRaidTargetIndex will return nil.
+                    sleep(1)
                     for i=1, members-1 do
                         local randomIndexOfTable = math.random(tablelength(unused_markers))
                         if GetRaidTargetIndex("party"..i) == nil then
-                            print("RandomIndexOfTable: " .. randomIndexOfTable .. "\n The extra marks should be on aswell!")
                             SetRaidTarget("party"..i, unused_markers[randomIndexOfTable])
                             table.remove(randomIndexOfTable)
                         end
@@ -124,4 +108,4 @@ local function markAndRaidAssistTeammates(self, event, ...)
     end
 end
 
-frame:SetScript("OnEvent", markAndRaidAssistTeammates)
+frame:SetScript("OnEvent", markTeammatesAndSelf)
