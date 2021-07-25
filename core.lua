34c54a7e645423f930f8cc4710c52aefa2c333e3
brandongevat <brandongevat@gmail.com>
2021-07-25 06:06:41 -0400

    Add mark pets Simplify code Added efficiency

--- core.lua
+++ core.lua
@@ -27,106 +27,90 @@ local unused_markers = {
     ["skull"] = 8
 }
 
-function removeKey(table, key)
-    local element = table[key]
-    table[key] = nil
-    return element
+local relatives = {
+    ["ROGUE"] = "star",
+    ["DRUID"] = "circle",
+    ["WARLOCK"] = "diamond",
+    ["PALADIN"] = "diamond",
+    ["HUNTER"] = "triangle",
+    ["MAGE"] = "moon",
+    ["SHAMAN"] = "square",
+    ["WARRIOR"] = "cross",
+    ["PRIEST"] = "skull"
+}
+
+local function removeValue(table, value)
+    local key = table[value]
+    table[value] = nil
+    return key
+end
+
+
+local function findUsableMark(table, target)
+    local marker = ""
+    for k,v in pairs(table) do
+        if v ~= nil then
+            marker = k
+            break
+        end
+    end
+    SetRaidTarget(target, table[marker])
+    removeValue(table, marker)
 end
 
 
 local function setRaidTargetByClass(target, ...)
     local _, englishClass, _ = UnitClass(target);
-    
-    if englishClass == "ROGUE" then
-        if unused_markers["star"] then
-            SetRaidTarget(target, 1)
-            removeKey(unused_markers, "star")
+    for k,v in pairs(relatives) do
+        if k == englishClass then
+            if unused_markers[v] then
+                SetRaidTarget(target, unused_markers[v])
+                removeValue(unused_markers, v)
+                break
+            else
+                findUsableMark(unused_markers, target)
+                break
+            end
         end
     end
-    if englishClass == "DRUID" then
-        if unused_markers["circle"] then
-            SetRaidTarget(target, 2)
-            removeKey(unused_markers, "circle")
-        end 
+end
+
+local function markPlayers(self, event, members)
+    if members > 1 then
+        if UnitIsGroupLeader("player") or UnitIsGroupAssistant("player") then
+            ConvertToRaid()
+            -- mark self
+            if not GetRaidTargetIndex("player") then
+                print("[ArenaMarker]: Marking the group.")
+                setRaidTargetByClass("player")
+            end
+            -- mark party members
+            for i=1, members-1 do
+                if not GetRaidTargetIndex("party"..i) then
+                    setRaidTargetByClass("party"..i)
+                end
+            end
+        end
     end
-    if englishClass == "WARLOCK" then
-        if unused_markers["diamond"] then
-            SetRaidTarget(target, 3)
-            removeKey(unused_markers, "diamond")
-        end 
-    end
-    if englishClass == "PALADIN" then
-        if unused_markers["diamond"] then
-            SetRaidTarget(target, 3)
-            removeKey(unused_markers, "diamond")
-        end 
-    end
-    if englishClass == "HUNTER" then
-        if unused_markers["triangle"] then
-            SetRaidTarget(target, 4)
-            removeKey(unused_markers, "triangle")
-        end 
-    end
-    if englishClass == "MAGE" then
-        if unused_markers["moon"] then
-            SetRaidTarget(target, 5)
-            removeKey(unused_markers, "moon")
-        end 
-    end
-    if englishclass == "SHAMAN" then
-        if unused_markers["square"] then
-            SetRaidTarget(target, 6)
-            removeKey(unused_markers, "square")
-        end 
-    end
-    if englishClass == "WARRIOR" then
-        if unused_markers["cross"] then
-            SetRaidTarget(target, 7)
-            removeKey(unused_markers, "cross")
-        end 
-    end
-    if englishClass == "PRIEST" then
-        if unused_markers["skull"] then
-            SetRaidTarget(target, 8)
-            removeKey(unused_markers, "skull")
-        end 
+end
+
+local function markPets(self, event, members)
+    for i=1, members-1 do
+        if not GetRaidTargetIndex("party"..i.."pet") then
+            findUsableMark(unused_markers, "party"..i.."pet")
+            break
+        end
     end
 end
 
 local function markTeammatesAndSelf(self, event, ...)
     if event == "CHAT_MSG_BG_SYSTEM_NEUTRAL" then
         arg1 = ...
-        if string.find(arg1, "One minute until the Arena battle begins!") or string.find(arg1, "Thirty seconds until the Arena battle begins!") or string.find(arg1, "Fifteen seconds until the Arena battle begins!") or string.find(arg1, "The Arena battle has begun!") then
-            local members = GetNumGroupMembers()
-            if members > 1 then
-                if UnitIsGroupLeader("player") then
-                    ConvertToRaid()
-                    -- mark self
-                    if GetRaidTargetIndex("player") == nil then
-                        print("[ArenaMarker]: Marking the group.")
-                        setRaidTargetByClass("player")
-                    end
-                    -- mark party members
-                    for i=1, members-1 do
-                        if GetRaidTargetIndex("party"..i) == nil then
-                            setRaidTargetByClass("party"..i)
-                        end
-                    end
-                    -- mark duplicate class members
-                    local marker = ""
-                    for j=1, members-1 do
-                        if GetRaidTargetIndex("party"..j) == nil then
-                            for i,v in pairs(unused_markers) do
-                                if v ~= nil then
-                                    marker = i
-                                    break
-                                end
-                            end
-                            SetRaidTarget("party"..j, unused_markers[marker])
-                        end
-                    end
-                end
-            end
+        local members = GetNumGroupMembers()
+        if string.find(arg1, "One minute until the Arena battle begins!") or string.find(arg1, "Thirty seconds until the Arena battle begins!") or string.find(arg1, "Fifteen seconds until the Arena battle begins!") then
+            markPlayers(self, event, members)
+        elseif string.find(arg1, "The Arena battle has begun!") then
+            markPets(self, event, members)
         end
     end
 end
