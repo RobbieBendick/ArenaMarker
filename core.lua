21e3dba70bc2bc92adf5f04144aab0b0c975b537
brandongevat <brandongevat@gmail.com>
2021-07-25 06:16:10 -0400

    Quality of life

--- core.lua
+++ core.lua
@@ -5,26 +5,26 @@ frame:RegisterEvent("CHAT_MSG_BG_SYSTEM_NEUTRAL")
 
 --[[
     Marker numbers:
-        1 = Yellow 4-point Star; Rogue
-        2 = Orange Circle; Druid
-        3 = Purple Diamond; Lock, Paladin
-        4 = Green Triangle; Hunter
-        5 = White Crescent Moon; Mage
-        6 = Blue Square; Shaman
-        7 = Red "X" Cross; Warrior
-        8 = White Skull; Priest
+        2 = Yellow 4-point Star; Rogue
+        3 = Orange Circle; Druid
+        4 = Purple Diamond; Lock, Paladin
+        5 = Green Triangle; Hunter
+        6 = White Crescent Moon; Mage
+        7 = Blue Square; Shaman
+        8 = Red "X" Cross; Warrior
+        9 = White Skull; Priest
 --]]
 
 
 local unused_markers = {
-    ["star"] = 1,
-    ["circle"] = 2,
-    ["diamond"] = 3,
-    ["triangle"] = 4,
-    ["moon"] = 5,
-    ["square"] = 6,
-    ["cross"] = 7,
-    ["skull"] = 8
+    ["star"] = 2,
+    ["circle"] = 3,
+    ["diamond"] = 4,
+    ["triangle"] = 5,
+    ["moon"] = 6,
+    ["square"] = 7,
+    ["cross"] = 8,
+    ["skull"] = 9
 }
 
 local relatives = {
@@ -76,26 +76,24 @@ local function setRaidTargetByClass(target, ...)
 end
 
 local function markPlayers(members)
-    if members > 1 then
-        if UnitIsGroupLeader("player") or UnitIsGroupAssistant("player") then
-            ConvertToRaid()
-            -- mark self
-            if not GetRaidTargetIndex("player") then
-                print("[ArenaMarker]: Marking the group.")
-                setRaidTargetByClass("player")
-            end
-            -- mark party members
-            for i=1, members-1 do
-                if not GetRaidTargetIndex("party"..i) then
-                    setRaidTargetByClass("party"..i)
-                end
+    if members > 2 then
+        ConvertToRaid()
+        -- mark self
+        if not GetRaidTargetIndex("player") then
+            print("[ArenaMarker]: Marking the group.")
+            setRaidTargetByClass("player")
+        end
+        -- mark party members
+        for i=2, members-1 do
+            if not GetRaidTargetIndex("party"..i) then
+                setRaidTargetByClass("party"..i)
             end
         end
     end
 end
 
 local function markPets(members)
-    for i=1, members-1 do
+    for i=2, members-1 do
         if not GetRaidTargetIndex("party"..i.."pet") then
             findUsableMark(unused_markers, "party"..i.."pet")
             break
@@ -103,16 +101,18 @@ local function markPets(members)
     end
 end
 
-local function markTeammatesAndSelf(self, event, ...)
+local function inArena(self, event, ...)
     if event == "CHAT_MSG_BG_SYSTEM_NEUTRAL" then
-        arg1 = ...
-        local members = GetNumGroupMembers()
-        if string.find(arg1, "One minute until the Arena battle begins!") or string.find(arg1, "Thirty seconds until the Arena battle begins!") or string.find(arg1, "Fifteen seconds until the Arena battle begins!") then
-            markPlayers(members)
-        elseif string.find(arg1, "The Arena battle has begun!") then
-            markPets(members)
+        if UnitIsGroupLeader("player") or UnitIsGroupAssistant("player") then
+            arg2 = ...
+            local members = GetNumGroupMembers()
+            if string.find(arg2, "One minute until the Arena battle begins!") or string.find(arg1, "Thirty seconds until the Arena battle begins!") or string.find(arg1, "Fifteen seconds until the Arena battle begins!") then
+                markPlayers(members)
+            elseif string.find(arg2, "The Arena battle has begun!") then
+                markPets(members)
+            end
         end
     end
 end
 
-frame:SetScript("OnEvent", markTeammatesAndSelf)
+frame:SetScript("OnEvent", inArena)
